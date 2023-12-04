0ec57d5e774cc66aefad75e564e3d043215e6656
aliensen36 <142628769+aliensen36@users.noreply.github.com>
2023-12-04 21:10:33 +0300

    D6.6

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -5,7 +5,7 @@ from .models import Post
 
 class PostList(ListView):
     model = Post
-    ordering = 'created_date'
+    ordering = '-created_date'
     template_name = 'news.html'
     context_object_name = 'news'
 
