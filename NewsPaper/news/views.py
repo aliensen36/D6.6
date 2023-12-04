5d40a79a5c9f197ab077a360bc603dc7d09c0196
aliensen36 <142628769+aliensen36@users.noreply.github.com>
2023-12-04 17:46:00 +0300

    D6.6

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,20 +1,18 @@
-# Импортируем класс, который говорит нам о том,
-# что в этом представлении мы будем выводить список объектов из БД
+from datetime import datetime
 from django.views.generic import ListView, DetailView
 from .models import Post
 
 
 class PostList(ListView):
-    # Указываем модель, объекты которой мы будем выводить
     model = Post
-    # Поле, которое будет использоваться для сортировки объектов
     ordering = 'created_date'
-    # Указываем имя шаблона, в котором будут все инструкции о том,
-    # как именно пользователю должны быть показаны наши объекты
     template_name = 'news.html'
-    # Это имя списка, в котором будут лежать все объекты.
-    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
 
 class PostDetail(DetailView):
     # Модель всё та же, но мы хотим получать информацию по отдельному товару
